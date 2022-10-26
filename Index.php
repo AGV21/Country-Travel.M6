2b9b26c7fb1b9f9856c6bbeb001e812f15efe272
Alex <alexgv2122@gmail.com>
2022-10-26 10:09:32 -0400

    Connected text box from site to database to store entries. Closed the connection to sql server at the end of code

--- Index.php
+++ Index.php
@@ -7,6 +7,8 @@
                 exit("Database failed");
             }
 
+    $userid = 1;
+
     $CountryName = mysqli_query($databaseConnection, "SELECT * FROM countries");
     if( !$CountryName ){
         echo( mysqli_error($databaseConnection) );
@@ -15,6 +17,33 @@
     }
 
     // exit();
+    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['postButton'])){
+        $content = mysqli_real_escape_string($databaseConnection, $_POST['content']);
+
+        if(!isset($content) || trim($content) === ""){
+            $errors[] = "Type Review";
+        }
+        if(empty($errors )){
+            $sql = 'INSERT INTO post';
+            $sql .= '(content, userid)';
+            $sql .= 'VALUE(';
+            $sql .= "'" . $content . "', ";
+            $sql .= "'" . $userid . "'";
+            $sql .= ')';
+
+        $postSavedSuccess = mysqli_query($databaseConnection, $sql);
+
+        if($postSavedSuccess){
+         } else{
+                echo(mysqli_error($databaseConnection));
+                if($databaseConnection){
+                    mysqli_close($databaseConnection);
+                }
+                exit();
+            }
+        }
+        
+    }
 
 ?>
 <DOC! html>
@@ -26,7 +55,7 @@
     <body id="mainContainer">
         <div class="trans.box">
             <p>Visited? Leave a review</p>
-            <form method="post">
+            <form action="index.php" method="post">
                 <select>
                     <option value='$name'>Pick Country</option>
 
@@ -43,9 +72,42 @@
                     }
                     ?>
                 </select><br>
-                <input type="text"><br>
-                <input type='submit' value='Post'>
+                <textarea name="content"></textarea><br>
+                <input type='submit' value='Post' name='postButton'>
             </form>
+            <?php
+                $sql = "SELECT * FROM post";
+
+                $allPost = mysqli_query($databaseConnection, $sql);
+
+                while($currentPost = mysqli_fetch_assoc($allPost)){
+            ?>
+                <article> 
+                    <?php //echo($currentPost['Date']);?> :
+                    <?php echo(htmlspecialchars($currentPost['content'])); ?> by
+                    <?php
+                       $sql = "SELECT * FROM users ";
+                       $sql .= "WHERE id='" . $currentPost['userid'] . "'";
+ 
+                       $userOfPost = mysqli_query($databaseConnection, $sql);
+                       $userOfPost = mysqli_fetch_assoc($userOfPost);
+                    ?>  
+                    <?php echo($userOfPost['nickname']); ?>
+                    <?php
+                    if($userOfPost['id'] == $userid){
+                    ?>
+                    <?php
+                    }
+                    ?>
+                </article>
+            <?php
+                }
+            ?>
         </div>
     </body> 
 </html>
+<?php
+    if($databaseConnection){
+        mysqli_close($databaseConnection);
+    }
+?>
